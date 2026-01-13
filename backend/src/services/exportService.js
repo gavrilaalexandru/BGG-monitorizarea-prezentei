0ae0d83695ce49394ac2c6e6a117958f08fa5a83
gavrilaalexandru <alexandruvalentingavrila@gmail.com>
2026-01-14 00:27:17 +0200

    export service

---
+++ backend/src/services/exportService.js
@@ -0,0 +1,33 @@
+const XLSX = require("xlsx");
+
+exports.exportToCSV = (attendances) => {
+  let csv = "Event Name,Participant Name,Participant Email,Check-in Time\n";
+
+  attendances.forEach((att) => {
+    csv += `"${att.event.name}","${att.participant.name}","${att.participant.email}","${att.checkInTime}"\n`;
+  });
+
+  return csv;
+};
+
+exports.exportToXLSX = async (eventGroup) => {
+  const workbook = XLSX.utils.book_new();
+
+  eventGroup.events.forEach((event) => {
+    const data = event.attendances.map((att) => ({
+      "Event Name": event.name,
+      "Participant Name": att.participant.name,
+      "Participant Email": att.participant.email,
+      "Check-in Time": att.checkInTime.toISOString(),
+    }));
+
+    const worksheet = XLSX.utils.json_to_sheet(data);
+    XLSX.utils.book_append_sheet(
+      workbook,
+      worksheet,
+      event.name.substring(0, 31),
+    );
+  });
+
+  return XLSX.write(workbook, { type: "buffer", bookType: "xlsx" });
+};
