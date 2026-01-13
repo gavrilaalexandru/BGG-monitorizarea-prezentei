6654671865bae32896ef0467b55d30e3ef49e62b
gavrilaalexandru <alexandruvalentingavrila@gmail.com>
2026-01-14 01:13:18 +0200

    more prisma deps

--- backend/src/prisma.js
+++ backend/src/prisma.js
@@ -1,5 +1,10 @@
+require("dotenv").config();
 const { PrismaClient } = require("@prisma/client");
+const { PrismaBetterSqlite3 } = require("@prisma/adapter-better-sqlite3");
 
-const prisma = new PrismaClient();
+const connectionString = process.env.DATABASE_URL;
+
+const adapter = new PrismaBetterSqlite3({ url: connectionString });
+const prisma = new PrismaClient({ adapter });
 
 module.exports = prisma;
