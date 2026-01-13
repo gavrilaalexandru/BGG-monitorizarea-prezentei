f4a4729f3fa99e01d0a8fa191176426381192a0c
gavrilaalexandru <alexandruvalentingavrila@gmail.com>
2026-01-13 23:53:58 +0200

    user controller

---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,45 @@
+const prisma = require("../prisma");
+
+exports.createUser = async (req, res) => {
+  try {
+    const { name, email, role } = req.body;
+
+    const user = await prisma.user.create({
+      data: { name, email, role },
+    });
+
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.getUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await prisma.user.findUnique({
+      where: { id },
+      include: {
+        eventGroups: true,
+        attendances: true,
+      },
+    });
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
